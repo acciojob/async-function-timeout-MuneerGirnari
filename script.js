c5cb62c7663348dce258235c4eb4fff940a8c5a7
Muneer Girnari <muneeragirnari@gmail.com>
2024-10-17 20:47:48 +0530

    solved version of JS

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+
+let text = document.getElementById("text");
+let delay = document.getElementById("delay");
+
+let buttn = document.getElementById("btn");
+let container = document.getElementById("output");
+
+buttn.addEventListener("click",callme);
+
+async function callme(e){
+    e.preventDefault();
+    let txtValue = text.value;
+    let txtDelay = delay.value;
+    text.value = "";
+    delay.value = "";
+    
+    await delayCall(txtDelay);
+    container.append(txtValue);
+}
+
+function  delayCall(txtDelay){
+    return new Promise ((resolve) => {
+        setTimeout(() =>{
+            resolve();
+        },txtDelay)
+    })
+}
